e9a627caca9c742b8df20552e29a8df3d4180d9c
Mark J Price <markjprice@gmail.com>
2016-02-12 08:55:05 +0000

    Final draft updates to Chapter 1

---
+++ Chapter01/Ch01_DotNetCore/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Ch01_DotNetCore
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome, .NET Core!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Chapter01/Ch01_DotNetCore/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Ch01_DotNetCore")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Ch01_DotNetCore")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1e23800b-a42d-4b54-88db-394edbfbe580")]
